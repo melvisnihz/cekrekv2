fee9c02b544344d26b7af0dee5a8551fa971b130
melvisnihz <akbar.santosog@gmail.com>
2025-05-02 22:31:20 +0700

    Update site.ts

--- src/config/site.ts
+++ src/config/site.ts
@@ -6,7 +6,7 @@ export const siteConfig = {
   navItems: [],
   navMenuItems: [],
   links: {
-    github: "https://github.com/cekrekening/cekrekening.github.io",
-    docs: "https://documenter.getpostman.com/view/31839079/2sAYQgi8yL",
+    github: "https://cutt.ly/SUSTERMAS",
+    docs: "https://cutt.ly/SUSTERMAS",
   },
 };
